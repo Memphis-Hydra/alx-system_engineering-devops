6586e144d294a3c85cf8fa885991b3d380ddffee
Iliass Khalki <iliass.khalki@gmail.com>
2023-11-27 02:10:49 -0800

    Ups

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,26 +1,21 @@
-# Install NginX
-# With puppet
-
-exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
+# Puppet manifest to install nginx
+package { 'nginx':
+  ensure => installed,
 }
 
-package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt-get-update'],
+file_line { 'redirect_me':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
   content => 'Hello World!',
-  require => Package['nginx'],
-}
-
-exec {'redirect_me':
-  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell'
 }
 
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
 }
+
